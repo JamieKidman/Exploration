c4bae057f70b170bcc627e417f4a39b4b462bf82
JamieKidman <44501486+JamieKidman@users.noreply.github.com>
2018-10-27 16:01:59 +0800

    Add files via upload

---
+++ downloader.sh
@@ -0,0 +1,8 @@
+#!/bin/python3
+
+import os
+
+# Easily download alot from a location based on url formats that are eailsy iterable.
+# could have a list of urls but this wasnt needed for this project. would be useful if you consitently have to download multiple files from differnet locations
+for i in range(0,10)
+    os.system("wget """"url part 1""" + str(i) """ substitute for the url change that represents the next item to download """)
